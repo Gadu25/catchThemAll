4042afd92920b08f1c180e277273e97b7600f39c
Alexanderr Udag <alexanderudag@gmail.com>
2024-11-15 16:26:46 +0800

    adjustment on list

--- pages/pokemon/[id].vue
+++ pages/pokemon/[id].vue
@@ -25,7 +25,7 @@
           </div>
           <div class="flex-1 text-start text-pokeball-black dark:text-pokeball-white">
             <div class="flex justify-start mb-2 align-center">
-              <h1 class="first-letter:uppercase text-4xl font-bold">{{ pokemon.name }}</h1>
+              <h1 class="first-letter:uppercase text-7xl font-bold">{{ pokemon.name }}</h1>
               <div class="my-auto mx-3 text-gray-500 text-xl cursor-pointer hover:scale-105 hover:text-gray-400 transition-all" @click="playCry()">
                 <i class="fa-solid fa-volume-high"></i>
               </div>
