ddc375aa1a60ac0e988d293c4fdff18f90f2fbf2
Alexanderr Udag <alexanderudag@gmail.com>
2024-11-26 15:47:27 +0800

    adjusted responsiveness

--- components/pokedexswipping.vue
+++ components/pokedexswipping.vue
@@ -114,7 +114,6 @@ export default {
             z-index: 10;
             box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
         }
-
     }
 }
 
--- pages/pokedex.vue
+++ pages/pokedex.vue
@@ -2,7 +2,7 @@
     <div class="py-4 xl:px-32 lg:px-20 sm:px-10 px-4">
         <div class="flex justify-start items-center">
             <img class="w-16" src="~/assets/images/pokedex.png">
-            <h2 class="font-extrabold text-5xl ms-2">Pokedex</h2>
+            <h2 class="font-extrabold text-2xl lg:text-5xl ms-2">Pokedex</h2>
         </div>
         <PokedexSwipping/>
     </div>
