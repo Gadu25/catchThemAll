b2851c646006a5c4c7de90a8392bbefa0bcc4aa9
Gadu25 <alexanderudag@gmail.com>
2024-11-28 15:58:48 +0800

    added infinit scroll feature

--- layouts/pokemonList.vue
+++ layouts/pokemonList.vue
@@ -1,23 +1,14 @@
 <template>
     <div>
-        <div class="flex flex-wrap">
-            <template v-for="pokemon in pokemons.results">
+        <div class="pokemon-container flex flex-wrap border rounded-lg p-2" @scroll="onScroll">
+            <template v-for="pokemon in pokemons">
                 <div class="w-full sm:w-1/2 lg:w-1/3 xl:w-1/5 p-2 card-wrapper" ref="card">
                     <Card :name="pokemon.name" :url="pokemon.url" @handleParentFunction="handleParentFunction"/>
                 </div>
             </template>
         </div>
-
-        <!-- nav -->
-        <div class="flex justify-end">
-            <div class="flex justify-center">
-                <button v-if="pokemons.previous != null" class="border rounded me-2 p-3 cursor-pointer hover:bg-pokemon-blue hover:text-pokeball-white" @click="paginatePrev()">
-                    <i class="fa-solid" :class="loading ? 'fa-spinner fa-spin': 'fa-chevron-left'"></i>
-                </button>
-                <button v-if="pokemons.next != null" class="border rounded p-3 cursor-pointer hover:bg-pokemon-blue hover:text-pokeball-white" @click="paginateNext()">
-                    <i class="fa-solid" :class="loading ? 'fa-spinner fa-spin': 'fa-chevron-right'"></i>
-                </button>
-            </div>
+        <div v-if="loading" class="flex justify-end">
+            <p>looking for pokemons..</p>
         </div>
     </div>
 </template>
@@ -31,50 +22,44 @@
         name: 'PokemonList',
         components: { Card },
         computed: {
-            ...mapState(usePokemonsStore, ['pokemons', 'loading', 'error']),
+            ...mapState(usePokemonsStore, ['pokemons', 'loading', 'nextUrl', 'error']),
         },
         methods: {
             ...mapActions(usePokemonsStore, ['fetchPokemons']),
-            handleFetch() {
-                this.fetchPokemons();
-            },
-            paginatePrev(){
-                if(this.pokemons.previous){
-                    this.fetchPokemons(this.pokemons.previous);
-
-                }
-            },
-            paginateNext(){
-                if(this.pokemons.next){
-                    this.fetchPokemons(this.pokemons.next);
-                }
-            },
             handleParentFunction(){
                 this.$emit('failedCatch');
+            },
+            onScroll(e) {
+                const { scrollTop, scrollHeight, clientHeight } = e.target;
+                if (scrollHeight - scrollTop <= clientHeight * 1.5) {
+                    this.fetchPokemons(); 
+                }
             }
         },
         watch: {
-            pokemons(newVal, oldVal){
-                if (newVal.results.length > 0) {
-                    this.$nextTick(() => {
-                        const cards = this.$refs.card;
-                        for(let card of cards){
-                            card.classList.add('animate-fadeInLoad')
-                        }
-                    })
-                }
-            }
+            // pokemons(newVal, oldVal){
+            //     if (newVal.results.length > 0) {
+            //         this.$nextTick(() => {
+            //             const cards = this.$refs.card;
+            //             for(let card of cards){
+            //                 card.classList.add('animate-fadeInLoad')
+            //             }
+            //         })
+            //     }
+            // }
         },
         mounted() {
-            
+            this.fetchPokemons()
         },
         created() {
-            if(isEmpty(this.pokemons)){
-                this.handleFetch();
-            }
         },
     }
 </script>
 
 <style lang="scss" scoped>
+.pokemon-container {
+    max-height: 65vh; 
+    overflow: auto; 
+    scroll-behavior: smooth;
+}
 </style>
--- store/pokemons.js
+++ store/pokemons.js
@@ -4,17 +4,21 @@ import axios from 'axios';
 import _ from 'lodash';
 
 export const usePokemonsStore = defineStore('pokemons', () => {
-  const pokemons = ref({});
+  const pokemons = ref([]); // Array to hold all fetched Pokémon
+  const nextUrl = ref('https://pokeapi.co/api/v2/pokemon?limit=20'); // URL for the next page
   const loading = ref(false);
   const error = ref(null);
 
-  // Fetch function with Lodash to limit API calls (e.g., throttling or debouncing)
-  const fetchPokemons = _.debounce(async (url='https://pokeapi.co/api/v2/pokemon?limit=15') => {
+  // Fetch function with Lodash debounce
+  const fetchPokemons = _.debounce(async () => {
+    if (!nextUrl.value || loading.value) return; // Prevent multiple calls or if no more data
     loading.value = true;
     error.value = null;
+
     try {
-      const response = await axios.get(url);
-      pokemons.value = response.data;
+      const response = await axios.get(nextUrl.value);
+      pokemons.value.push(...response.data.results); // Append new Pokémon to the list
+      nextUrl.value = response.data.next; // Update next page URL
     } catch (err) {
       error.value = err.message || 'Failed to fetch data';
     } finally {
@@ -24,6 +28,7 @@ export const usePokemonsStore = defineStore('pokemons', () => {
 
   return {
     pokemons,
+    nextUrl,
     loading,
     error,
     fetchPokemons,
